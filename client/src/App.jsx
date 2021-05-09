59ec0202e6779f98f43f87166b3b6e2ca4c47fef
5542335 <bosy902@gmail.com>
2021-05-09 21:44:31 +0300

    adaptation added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,10 +8,20 @@ import GoogleLogin from "react-google-login";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 import Agreements from "./component/registerForm/body/agreement";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core";
 
 const responseGoogle = (response) => {
   console.log(response);
 };
+const useStyles = makeStyles((theme) => ({
+  grid: {
+    width: "100%",
+    margin: "0px",
+    justifyContent: "center",
+  },
+}));
 
 const SignupForm = () => {
   const formik = useFormik({
@@ -46,59 +56,83 @@ const SignupForm = () => {
     onSubmit: (values) => {},
   });
 
+  const classes = useStyles();
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className="form">
         <form onSubmit={formik.handleSubmit}>
-          <TitleRegisterForm titleText="Регистрация" />
+          <Grid container className={classes.grid}>
+            <TitleRegisterForm titleText="Регистрация" />
+          </Grid>
+          <Grid container spacing={1} className={classes.grid}>
+            <Grid item xs={12} sm={7} md={5}>
+              <CustomTextField
+                id="firstName"
+                label="Имя"
+                name="firstName"
+                type="text"
+                autoComplete="Имя"
+                helperText=""
+                formik={formik}
+              />
+            </Grid>
+            <Grid item xs={12} sm={7} md={5}>
+              <CustomTextField
+                id="lastName"
+                label="Фамилия"
+                name="lastName"
+                type="text"
+                autoComplete="Фамилия"
+                helperText=""
+                formik={formik}
+              />
+            </Grid>
 
-          <CustomTextField
-            id="firstName"
-            label="Имя"
-            name="firstName"
-            type="text"
-            autoComplete="Имя"
-            helperText=""
-            formik={formik}
-          />
-          <CustomTextField
-            id="lastName"
-            label="Фамилия"
-            name="lastName"
-            type="text"
-            autoComplete="Фамилия"
-            helperText=""
-            formik={formik}
-          />
-          <CustomTextField
-            id="password"
-            label="Пароль"
-            name="password"
-            type="password"
-            autoComplete="Пароль"
-            helperText=""
-            formik={formik}
-          />
-          <CustomTextField
-            id="email"
-            label="Почта"
-            name="email"
-            type="email"
-            autoComplete="Почта"
-            helperText=""
-            formik={formik}
-          />
-          <Agreements />
+            <Grid item xs={12} sm={7} md={5}>
+              <CustomTextField
+                id="password"
+                label="Пароль"
+                name="password"
+                type="password"
+                autoComplete="Пароль"
+                helperText=""
+                formik={formik}
+              />
+            </Grid>
+            <Grid item xs={12} sm={7} md={5}>
+              <CustomTextField
+                id="email"
+                label="Почта"
+                name="email"
+                type="email"
+                autoComplete="Почта"
+                helperText=""
+                formik={formik}
+              />
+            </Grid>
+          </Grid>
+          <Grid container className={classes.grid}>
+            <Grid item>
+              <Agreements />
+            </Grid>
+          </Grid>
 
-          <SubmitButton color="primary" submitButtonText="Зарегистрироваться" />
+          <Grid container spacing={1} className={classes.grid}>
+            <Grid item xs={6}>
+              <SubmitButton
+                color="primary"
+                submitButtonText="Зарегистрироваться"
+              />
 
-          <GoogleLogin
-            clientId="631572139627-994glqmtsdnvjkaf5g7qo450mvhptbb5.apps.googleusercontent.com"
-            buttonText="Войти через Google"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-          />
+              <GoogleLogin
+                clientId="631572139627-994glqmtsdnvjkaf5g7qo450mvhptbb5.apps.googleusercontent.com"
+                buttonText="Sigh up"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+              />
+            </Grid>
+          </Grid>
         </form>
       </div>
     </Container>
--- client/src/component/registerForm/title/Title.jsx
+++ client/src/component/registerForm/title/Title.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
 
-const TitleRegisterForm = ({ component = "h1", variant = "h5", titleText }) => {
+const TitleRegisterForm = ({
+  component = "h1",
+  variant = "h5",
+
+  titleText,
+}) => {
   return (
     <Typography component={component} variant={variant}>
       {titleText}
